4219ee53c39bed249395190a92c0c14117ed8e7a
Ciolou <57500839+Ciolou1@users.noreply.github.com>
2020-02-20 13:49:43 -0600

    includes
    Upload of includes folder and message about includes in the index.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- 
+    Author: Kettle Team
+    Last modified: 2/20/20
+    Last modified by: Sam Lohbeck
+ -->
+
+ <?php
+    // Include header (Hunter will be making a seperate file for the header)
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Home</title>
+ </head>
+ <body>
+     
+ </body>
+ </html>
