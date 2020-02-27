22e52e6ba7799922d35318aff979ac7a3044a14b
Ross Lowrance <55846368+lowrancr@users.noreply.github.com>
2020-02-27 12:30:21 -0600

    2nd Push

--- index.php
+++ index.php
@@ -4,18 +4,28 @@
     Last modified by: Sam Lohbeck
  -->
 
- <?php
-    // Include header (Hunter will be making a seperate file for the header)
- ?>
+ <?php 
+   $SERVER["DOCUMENT_ROOT"] = "C:\xampp\htdocs\kettleLMS"
+?>
 
  <!DOCTYPE html>
  <html lang="en">
  <head>
      <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge, chrome, firefox">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/canvas.css">
+    <script src="canvas.js"></script>
+    <link rel='icon' href='img/kettle.JPG' type='image/x-icon'/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
  </head>
  <body>
-     <?php echo("Blah") ?>
+	<?php include('/includes/navBar.php') ?>
+    <?php echo("Blah") ?>
  </body>
  </html>
