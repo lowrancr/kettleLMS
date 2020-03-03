8527373d095ab8a92aa1b6ac4408bdf6e1368a8f
Ciolou <57500839+Ciolou1@users.noreply.github.com>
2020-03-03 13:51:37 -0600

    Moved CSS files

---
+++ home.php
@@ -0,0 +1,21 @@
+<!-- 
+    Author: Kettle Team
+    Last modified: 3/03/20
+    Last modified by: Sam Lohbeck
+ -->
+
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    
+</body>
+</html>
