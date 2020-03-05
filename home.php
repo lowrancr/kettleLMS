55cce6ea5177f4bfeeed495603ff86c0eeaa57ca
Ciolou <57500839+Ciolou1@users.noreply.github.com>
2020-03-05 13:21:50 -0600

    Grid Setup

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <!-- 
     Author: Kettle Team
-    Last modified: 3/03/20
+    Last modified: 3/05/20
     Last modified by: Sam Lohbeck
  -->
 
@@ -12,26 +12,32 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="stylesheet" href="css/home.css">
     <title>Home</title>
 </head>
 <body>
     <div class="gridContainer">
         <header>
         <!-- Header -->
+        Header
         </header>
         <nav>
         <!-- Navigation -->
+        Navigation
         </nav>
         <main>
         <!-- Main content -->
+        Main
         </main>
         <section>
         <!-- Sidebar -->
+        Section
         </section>
         <footer>
         <!-- Footer -->
+        footer
         </footer>
-    </div>
+    </div> 
 </body>
 </html>
