88a6cbdec942edbacaf82436153027a8257b276f
oHRH <44187078+oHRH@users.noreply.github.com>
2020-03-12 15:24:17 -0500

    Fixed right side bar and implemented pictures

--- includes/rightSidebar.php
+++ includes/rightSidebar.php
@@ -1,5 +1,22 @@
- <nav id="sidebar-right">
+<link rel="stylesheet" href="css/rightsidebar.css">
+<nav id="sidebar-right">
         <div class="sidebar-header-right">
-            <h3><strong>Course Overview</strong></h3>
+            <h2><strong>Course Overview</strong></h2>
+        </div>
+        <div class="first-course">
+            <h3><strong>CIS-151</strong></h3>
+            <h3><strong>C# Progrcourseamming II</strong></h3>
+        </div>
+        <div class="second-course">
+            <h3><strong>CIS-120</strong></h3>
+            <h3><strong>Python Programming</strong></h3>
+        </div>
+        <div class="third-course">
+            <h3><strong>CIS-185</strong></h3>
+            <h3><strong>Database and Queries</strong></h3>
+        </div>
+        <div class="fourth-course">
+            <h3><strong>CIS-131</strong></h3>
+            <h3><strong>Web Development II</strong></h3>
         </div>
         </nav>
